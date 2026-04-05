867c4385ed2d77a27ed37df5223f5b2f0b88a2ba
AdarshRajDS <adarshrajdsc@gmail.com>
2026-04-05 17:15:18 +0200

    fix timeout issue

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,11 @@
+import os
 from pydantic_settings import BaseSettings
+
+TIMEOUT = int(os.getenv("TIMEOUT", 60))
 
 class Settings(BaseSettings):
     APP_NAME: str = "latex-pdf-service"
-    TIMEOUT: int = 15
+    TIMEOUT: int = TIMEOUT
     TMP_DIR: str = "/tmp/latex"
 
 settings = Settings()
